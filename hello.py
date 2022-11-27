dff96617a810af2a481be8b4715738c376bf7df7
Naziff <bellnas09@gmail.com>
2022-11-27 18:46:00 +0000

    test with example

--- hello.py
+++ hello.py
@@ -4,3 +4,15 @@ def add(x, y):
 
 result = add(1, 2)
 print(f"this is the sum of 1, 2, {result}")
+
+
+def toyou(x):
+    return f"hi {x}"
+
+
+def add2(x):
+    return x + 1
+
+
+def subtract(x):
+    return x - 1
